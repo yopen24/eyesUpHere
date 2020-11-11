e922861ba24beb0e372aac4d6b993e7f57f2ef5e
yopen24 <61549437+yopen24@users.noreply.github.com>
2020-11-11 14:27:40 -0300

    Componentes creados

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Rank = () => {
+  return (
+    <div>
+      <div class="white f3">{"Diego, your current rank iss.."}</div>
+      <div class="white f1">{"#5"}</div>
+    </div>
+  );
+};
+
+export default Rank;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'tachyons';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
