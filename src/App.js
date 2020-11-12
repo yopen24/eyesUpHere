f8ef52c5a9914fd12ac4226f967db34eed6b25df
yopen24 <61549437+yopen24@users.noreply.github.com>
2020-11-12 17:04:15 -0300

    AVANCE
    Sigue hacer el DOM manipulation de la funcion calculateFaceDetection

--- src/App.js
+++ src/App.js
@@ -23,27 +23,29 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box:{},
     };
   }
   onInputChange = (event) => {
     this.setState({input: event.target.value});
   };
 
+calculateFaceLocation = (data) =>{
+  const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+const image = document.getElementById('inputimage');
+  };
   onButtonSubmit = () => {
     
     this.setState({ imageUrl:this.state.input});
     app.models
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then(
-        function (response) {
-          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-          // do something with response
-        },
-        function (err) {
-          // there was an error
-        }
-      );
+      .then(response => 
+      this.calculateFaceLocation(response)) 
+      .catch(err => console.log(err)) ;
+        
+        
+      
   };
 
   render() {
