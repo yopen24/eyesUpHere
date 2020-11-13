18c8b40f319b2f1ac623766d017efb581e8c5363
yopen24 <61549437+yopen24@users.noreply.github.com>
2020-11-13 14:33:51 -0300

    added the signin, register and signout functionalities

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Particles from "react-particles-js";
 import Clarifai from "clarifai";
+import Signin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
@@ -14,7 +16,7 @@ const app = new Clarifai.App({
 
 const particlesOptions = {
   particles: {
-    number: { value: 30, density: { enable: true, value_area: 800 } },
+    number: { value: 80, density: { enable: true, value_area: 800 } },
   },
 };
 
@@ -25,6 +27,8 @@ class App extends Component {
       input: "",
       imageUrl: "",
       box: {},
+      route: "signin",
+      isSignedIn: false
     };
   }
   onInputChange = (event) => {
@@ -37,41 +41,61 @@ class App extends Component {
     const image = document.getElementById("inputimage");
     const width = Number(image.width);
     const height = Number(image.height);
-    console.log(width, height);
     return {
-      leftCol: clarifaiFace.left_col*width ,
-      topRow: clarifaiFace.top_row * height ,
-      rightCol: width - (clarifaiFace.right_col *width),
-      bottomRow: height - (clarifaiFace.bottom_row * height),
-      
-    }
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
   };
 
   displayFaceBox = (box) => {
-    console.log(box);
-    this.setState({box:box});
-  }
+    
+    this.setState({ box: box });
+  };
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
     app.models
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then((response) => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .then((response) =>
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      )
       .catch((err) => console.log(err));
   };
 
+  onRouteChange = (route) => {
+    if(route=== 'signout'){
+      this.setState({isSignedIn: false})
+    }else if(route==='home'){
+      this.setState({isSignedIn: true})
+    }
+
+    this.setState({ route: route});
+  };
+
   render() {
+    const {isSignedIn, imageUrl, route, box} = this.state;
     return (
       <div className="App">
         <Particles className="particles" params={particlesOptions} />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm
-          onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButtonSubmit}
-        />
-        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        {route === "home" ? 
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition
+              box={box}
+              imageUrl={imageUrl}
+            />
+          </div> : (route=== 'signin') ? 
+          <Signin onRouteChange={this.onRouteChange} />
+         : <Register onRouteChange={this.onRouteChange} />
+        } 
       </div>
     );
   }
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,8 +3,8 @@ import React from "react";
 const Rank = () => {
   return (
     <div>
-      <div class="white f3">{"Diego, your current rank iss.."}</div>
-      <div class="white f1">{"#5"}</div>
+      <div className="white f3">{"Diego, your current rank iss.."}</div>
+      <div className="white f1">{"#5"}</div>
     </div>
   );
 };
